4e82a031647ba4ba22b4ecf6dba87aefd0b00b7f
supanat <supanat.ap@gmail.com>
2021-02-15 16:48:29 +0700

    a

--- 105_lab3.1.c
+++ 105_lab3.1.c
@@ -1,8 +1,22 @@
 #include<stdio.h>
 int main()
-{   int a;
+{   int a[20][20],i,j;
 
+    for(i=0;i<4;i++)
+    {
+        for(j=0;j<4;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
 
+    for(i=0;i<4;i++)
+    {
+        for(j=0;j<4;j++)
+        {
+           printf("%d",&a[i][j]);
+        }
+    }
 
 
 
