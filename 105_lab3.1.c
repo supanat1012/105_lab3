927b8ac538134d08bc406d5b36a86c93e173b461
supanat <supanat.ap@gmail.com>
2021-02-15 17:03:48 +0700

    a

--- 105_lab3.1.c
+++ 105_lab3.1.c
@@ -1,24 +1,29 @@
 #include<stdio.h>
 int main()
-{   int a[20][20],i,j;
+{   int a[20][20],i,j,p[4]={0},max;
 
     for(i=0;i<4;i++)
     {
         for(j=0;j<4;j++)
         {
             scanf("%d",&a[i][j]);
+            p[i]=p[i]+a[i][j];
         }
     }
 
-    for(i=0;i<4;i++)
+   /* for(i=0;i<4;i++)
     {
-        for(j=0;j<4;j++)
+        for(j=0;j<3;j++)
         {
-           printf("%d.",a[i][j]);
+           p[i]=a[i][j]+a[i][j+1]+p[i];
         }
-        printf("\n");
-    }
 
+    }*/
+
+    for(j=0;j<4;j++)
+        {
+           printf("%d\n",p[j]);
+        }
 
 
 
