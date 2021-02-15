9e7c56db719070cec3b009cd3d9d1215d3d87699
supanat <supanat.ap@gmail.com>
2021-02-15 17:36:32 +0700

    Update 105_lab3.2.c

--- 105_lab3.2.c
+++ 105_lab3.2.c
@@ -1,7 +1,21 @@
 #include<stdio.>
 int main()
-{
+{   int a,i,j,n,ch[],hu[],;
 
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+    {
+        scanf("%d %d",&ch[i],&hu[i]);
+    }
+
+    for(i=0;i<=n;i++)
+    {
+        for(j=0;j<=n;j++)
+        {
+
+        }
+    }
 
 
 
