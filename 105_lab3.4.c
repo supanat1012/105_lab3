e34ba571d553596ab3895f9fad7cd3d629c94161
supanat <supanat.ap@gmail.com>
2021-02-15 18:16:39 +0700

    Create 105_lab3.4.c

---
+++ 105_lab3.4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,i,count=0;
+
+    scanf("%d",&a)
+
+    for(i=0;i<a;i++)
+    {
+        if(a%i==0)
+        {
+            count++;
+        }
+    }
+
+
+
+
+}
