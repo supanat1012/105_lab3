18472ca73f3e97458c19b9b468be2964d3572c1b
supanat <supanat.ap@gmail.com>
2021-02-15 17:21:48 +0700

    a

--- 105_lab3.1.c
+++ 105_lab3.1.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main()
-{   int a[20][20],i,j,p[4]={0},max;
+{   int a[20][20],i,j,p[5]={0,0,0,0,0},max;
 
     for(i=0;i<5;i++)
     {
@@ -29,18 +29,55 @@ int main()
             }
             else if(p[3]>p[4])
             {
-                printf("4 %d",p[3])
+                printf("4 %d",p[3]);
             }
         }
         else if(p[2]>p[3])
         {
             if(p[2]>p[4])
             {
-                printf("2 %d",p[2]);
+                printf("3 %d",p[2]);
             }
         }
+        else if(p[3]>p[4])
+        {
+            printf("4 %d",p[3]);
+        }
+        else
+            printf("5 %d",p[4]);
     }
+    else if(p[1]>p[2])
+    {
+        if(p[1]>p[3])
+            {
+                if(p[1]>p[4])
+                {
+                    printf("2 %d",p[1]);
+                }
+                else
+                {
+                    printf("5 %d",p[4]);
+                }
+            }
+        else if(p[3]>p[4])
+            {
+                printf("4 %d",p[3]);
+            }
 
+        else if(p[2]>p[3])
+        {
+            if(p[2]>p[4])
+            {
+                printf("3 %d",p[2]);
+            }
+        }
+        else if(p[3]>p[4])
+        {
+            printf("4 %d",p[3]);
+        }
+        else
+            printf("5 %d",p[4]);
+    }
 
 
 }
